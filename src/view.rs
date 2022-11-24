ff14f1b6c0a662306fb50fab35fe509829357609
kamecha <masakikameyama81@gmail.com>
2022-11-24 22:50:30 +0900

    カーソルはカーソルだけを描画するようにした

--- src/view.rs
+++ src/view.rs
@@ -30,27 +30,10 @@ pub fn view<T: std::io::Write>(
                     model::CardType::Empty => continue,
                 }
             }
-            if *cursor == (i, j) {
-                // cursorの描画
-                let cursor_base = (base.0 - 1, base.1 - 1);
-                let cursor_size = card_size + 2;
-                let cursor_col = cursor_base.0 + cursor.1 * cursor_size;
-                let cursor_row = cursor_base.1 + cursor.0 * cursor_size;
-                let tmp = ["||", "||"].join(&mark);
-                execute!(output, MoveTo(cursor_col, cursor_row),)?;
-                execute!(output, Print("+---+\n"))?;
-                execute!(output, MoveTo(cursor_col, cursor_row + 1),)?;
-                execute!(output, Print("|+-+|\n"))?;
-                execute!(output, MoveTo(cursor_col, cursor_row + 2),)?;
-                execute!(output, Print(tmp))?;
-                execute!(output, MoveTo(cursor_col, cursor_row + 3),)?;
-                execute!(output, Print("|+-+|\n"))?;
-                execute!(output, MoveTo(cursor_col, cursor_row + 4),)?;
-                execute!(output, Print("+---+\n"))?;
-            } else {
-                // cardの描画
-                let col = base.0 + j * (card_size + card_spacing);
-                let row = base.1 + i * (card_size + card_spacing);
+            // cardの描画
+            let col = base.0 + j * (card_size + card_spacing);
+            let row = base.1 + i * (card_size + card_spacing);
+            if game.model.get_card((i * width + j) as usize).card_type != model::CardType::Empty {
                 execute!(output, MoveTo(col, row),)?;
                 let tmp = ["|", "|"].join(&mark);
                 execute!(output, Print("+-+\n"))?;
@@ -58,6 +41,29 @@ pub fn view<T: std::io::Write>(
                 execute!(output, Print(tmp))?;
                 execute!(output, MoveTo(col, row + 2),)?;
                 execute!(output, Print("+-+\n"))?;
+            }
+            if *cursor == (i, j) {
+                // cursorの描画
+                let cursor_base = (base.0 - 1, base.1 - 1);
+                let cursor_size = card_size + 2;
+                let cursor_col = cursor_base.0 + cursor.1 * cursor_size;
+                let cursor_row = cursor_base.1 + cursor.0 * cursor_size;
+                execute!(output, MoveTo(cursor_col, cursor_row),)?;
+                execute!(output, Print("+---+\n"))?;
+                execute!(output, MoveTo(cursor_col, cursor_row + 1),)?;
+                execute!(output, Print("|\n"))?;
+                execute!(output, MoveTo(cursor_col + 4, cursor_row + 1),)?;
+                execute!(output, Print("|\n"))?;
+                execute!(output, MoveTo(cursor_col, cursor_row + 2),)?;
+                execute!(output, Print("|\n"))?;
+                execute!(output, MoveTo(cursor_col + 4, cursor_row + 2),)?;
+                execute!(output, Print("|\n"))?;
+                execute!(output, MoveTo(cursor_col, cursor_row + 3),)?;
+                execute!(output, Print("|\n"))?;
+                execute!(output, MoveTo(cursor_col + 4, cursor_row + 3),)?;
+                execute!(output, Print("|\n"))?;
+                execute!(output, MoveTo(cursor_col, cursor_row + 4),)?;
+                execute!(output, Print("+---+\n"))?;
             }
         }
         // execute!(std::io::stdout(), Print("\n"))?;
