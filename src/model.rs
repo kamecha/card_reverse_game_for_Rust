fe6cfbf9f4c53cadefa60bc598b78cab468dcab6
kamecha <masakikameyama81@gmail.com>
2022-11-24 22:52:16 +0900

    modelの初期化の充実とメソッドの追加

--- src/model.rs
+++ src/model.rs
@@ -10,6 +10,7 @@ pub enum CardType {
 #[derive(Copy, Clone, PartialEq)]
 pub struct Card {
     pub card_type: CardType,
+    // true: マークが見えない、false: マークが見える
     pub is_reverse: bool,
 }
 
@@ -25,8 +26,35 @@ impl Model {
                 Card {
                     card_type: CardType::Circle,
                     is_reverse: true,
-                };
-                8
+                },
+                Card {
+                    card_type: CardType::Circle,
+                    is_reverse: true,
+                },
+                Card {
+                    card_type: CardType::Square,
+                    is_reverse: true,
+                },
+                Card {
+                    card_type: CardType::Square,
+                    is_reverse: true,
+                },
+                Card {
+                    card_type: CardType::Triangle,
+                    is_reverse: true,
+                },
+                Card {
+                    card_type: CardType::Triangle,
+                    is_reverse: true,
+                },
+                Card {
+                    card_type: CardType::Cross,
+                    is_reverse: true,
+                },
+                Card {
+                    card_type: CardType::Cross,
+                    is_reverse: true,
+                },
             ],
         }
     }
@@ -35,6 +63,18 @@ impl Model {
     }
     pub fn set_card(&mut self, index: usize, card: Card) {
         self.cards[index] = card;
+    }
+    pub fn number_of_reverse_card(&self) -> usize {
+        let mut count = 0;
+        for card in &self.cards {
+            if card.is_reverse {
+                count += 1;
+            }
+        }
+        count
+    }
+    pub fn number_of_not_reverse_card(&self) -> usize {
+        self.cards.len() - self.number_of_reverse_card()
     }
     pub fn reverse_card(&mut self, index: usize) {
         self.cards[index].is_reverse = !self.cards[index].is_reverse;
